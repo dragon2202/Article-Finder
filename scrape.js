04aee148857872b08b715d75e758437f1c3a1a89
Calvin Tang <32778919+dragon2202@users.noreply.github.com>
2019-08-08 13:38:19 -0700

    Add files via upload

--- scrape.js
+++ scrape.js
@@ -7,14 +7,47 @@
   app.use(express.static('public'));
 
   app.get('/json', function(req, res){
-    const domain = '';
-
+    var offset = 0;
+    var index = 0;//0 for Fox 1 for CNet
+    //https://stackoverflow.com/questions/1216505/how-to-parse-a-string-in-javascript
+    var websites = [];
+    var FoxNews = {
+      domain: 'https://www.foxnews.com',
+      uri: 'https://www.foxnews.com/politics',
+      //num: 'div.article-list:nth-child(1) > article',
+      text: '.speakable',
+      date: '.article-date',
+      article: '.title > a',
+      //return $('div.article-list:nth-child(1) > article:nth-child(' + (i + 1) + ') div:nth-child(2)> header:nth-child(1) > h2:nth-child(2) > a:nth-child(1)')
+      isVideo: function ($, el){
+        var string = $(el).attr('href');
+        if(string.indexOf("https://video.foxnews.com/") > -1){
+          return true;
+        } else {
+          return false
+        }
+      }
+    }
+    var CNET = {
+      domain: 'https://www.cnet.com',
+      uri: 'https://www.cnet.com/news/',
+      num: 5,
+      text: '.speakableTextP1',
+      date: '.formattedDate',
+      article: 'a.assetHed',
+      isVideo: function ($, el){
+        return false;
+      }
+    }
+    websites.push(FoxNews);
+    websites.push(CNET);
+    const domain = websites[index].domain;
     /*
     first options for request_promise;
     transform is similar to creating a new Promise;
     */
     const options = {
-      uri: 'https://www.foxnews.com/',
+      uri: websites[index].uri,
       transform: function (body) {
         return cheerio.load(body);
       }
@@ -33,46 +66,40 @@
       var promises = []; //creating a new array of promises
       json.articles = [] ; //new array  articles
     
-    
-//    for(var i = 0; i < 15; i++){
-//      var article = $('div.article-list:nth-child(1) > article:nth-child(' + (i + 1) + ') div:nth-child(2)> header:nth-child(1) > h2:nth-child(2) > a:nth-child(1)');
-//     console.log(article.attr('href'));
-//    }
-    //var site2 = $('div.article-list:nth-child(1) > article:nth-child(2) > div:nth-child(2) > header:nth-child(1) > h2:nth-child(2) > a:nth-child(1)');
-    //var site = $('div.article-list:nth-child(1) > article:nth-child(1) > div:nth-child(2) > header:nth-child(1) > h2:nth-child(2) > a:nth-child(1)');
-    //var site2 = $('.m  .a').attr('href');
-    //console.log(site2);
     //https://stackoverflow.com/questions/250688/count-immediate-child-div-elements-using-jquery
-    var num = $('div.article-list:nth-child(1) > article').length;
-    console.log(num);
-    for(var i = 0; i < num; i++){
-      var article = $('div.article-list:nth-child(1) > article:nth-child(' + (i + 1) + ') div:nth-child(2)> header:nth-child(1) > h2:nth-child(2) > a:nth-child(1)');
-      let data = {}; //create new Object called data
-      data.title = $(article).text().trim(); //text of element and then trim trailing white spaces
-      let href = $(article).attr('href'); // link of element attribute 'href'
-      data.link = domain.concat(href);
-      //console.log(data.link);
-      /*
-      second options for request_promise -
-      cheerio will load data.link ;
-      select the text from <p class = '.speakableTextP1' ></p>;
-      put that text in data.text in json
-      */
-      const options2 = {
-        uri: data.link,
-        transform: function (body) {
-          const $ =  cheerio.load(body);
-          data.text = $('.speakable').text().trim();
-          data.date =  $('.article-date').text().trim();
-          return data;
-        }
-      };
-      /*
-      this will create promise for each
-      */
-      promises.push(request_promise(options2));
-        
-    };
+    $(websites[index].article).each((i,el) => {
+      if(i < 10 + offset){
+      if(websites[index].isVideo($, el) == true){
+        offset += 1;
+      } else {
+        let data = {}; //create new Object called data
+        data.title = $(el).text().trim(); //text of element and then trim trailing white spaces
+        let href = $(el).attr('href'); // link of element attribute 'href'
+        //console.log('HREF: ' + href);
+        data.link = domain.concat(href);
+        //console.log(data.link);
+        /*
+        second options for request_promise -
+        cheerio will load data.link ;
+        select the text from <p class = '.speakableTextP1' ></p>;
+        put that text in data.text in json
+        */
+        const options2 = {
+          uri: data.link,
+          transform: function (body) {
+            const $ =  cheerio.load(body);
+            data.text = $(websites[index].text).text().trim();
+            data.date =  $(websites[index].date).text().trim();
+            return data;
+          }
+        };
+        /*
+        this will create promise for each
+        */
+        promises.push(request_promise(options2));
+      }
+    }
+    });
 
       /*
       waiting for all promises to finish;
@@ -93,7 +120,7 @@
       })
     })
     .catch(function (error) {
-      console.log(error);
+      //console.log(error);
     });
 
   });
